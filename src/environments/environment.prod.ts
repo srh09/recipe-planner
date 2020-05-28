48fcf2372503566e433fd4e59f99da6b697af67c
SRH <srholand@gmail.com>
2020-05-27 19:44:26 -0500

    environment variables, yes you can expose these

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDVyPHegtplB9F2t3GPvFNffHfZ0_agmpo",
+    authDomain: "recipe-planner-f9e52.firebaseapp.com",
+    databaseURL: "https://recipe-planner-f9e52.firebaseio.com",
+    projectId: "recipe-planner-f9e52",
+    storageBucket: "recipe-planner-f9e52.appspot.com",
+    messagingSenderId: "678738905843",
+    appId: "1:678738905843:web:0a41a9763030ddf2a04586",
+    measurementId: "G-9K7MP5KJ8H"
+  }
 };
