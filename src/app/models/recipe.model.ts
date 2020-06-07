b747b1e95754cf5a6caf7ac215b6b5be00be7851
SRH <srholand@gmail.com>
2020-06-07 09:24:58 -0500

    model updated

--- src/app/models/recipe.model.ts
+++ src/app/models/recipe.model.ts
@@ -3,10 +3,10 @@ import { Ingredient } from "./ingredient.model"
 export class Recipe {
   constructor(
     public name: string,
-    public servings: string,
+    public servings: number,
     public description?: string,
-    public instructions?: string,
     public notes?: string,
+    public instructions?: string[],
     public ingredients?: (Ingredient | Recipe)[],
     public imagePath?: string,
     public amount?: number
