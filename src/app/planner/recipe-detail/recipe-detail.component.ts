b0f20188a84820f6e664e27faa0166ddf3c9ce28
SRH <srholand@gmail.com>
2020-06-07 19:59:05 -0500

    recipe general section finished

--- src/app/planner/recipe-detail/recipe-detail.component.ts
+++ src/app/planner/recipe-detail/recipe-detail.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from "@angular/core"
+import { FormGroup, Validators, FormBuilder, FormArray, FormControl } from "@angular/forms"
+import { BreakpointObserver, Breakpoints } from "@angular/cdk/layout"
 
 @Component({
   selector: "app-recipe-detail",
@@ -6,7 +8,45 @@ import { Component, OnInit } from "@angular/core"
   styleUrls: ["./recipe-detail.component.scss"],
 })
 export class RecipeDetailComponent implements OnInit {
-  constructor() {}
+  recipeDetailForm: FormGroup
 
-  ngOnInit() {}
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit() {
+    this.recipeDetailForm = this.formBuilder.group({
+      name: [null, [Validators.required]],
+      servings: [null, [Validators.required, Validators.pattern("^[1-9][0-9]?$")]],
+      description: [null],
+      instructions: [[]],
+      notes: [null],
+    })
+  }
+
+  onAddInstruction() {
+    ;(<FormArray>this.recipeDetailForm.get("instructions")).push(new FormControl(null))
+  }
+
+  onDeleteInstruction(index: number) {
+    ;(<FormArray>this.recipeDetailForm.get("instructions")).removeAt(index)
+  }
+
+  onSubmit() {
+    console.log(this.recipeDetailForm.value)
+  }
+
+  get name() {
+    return this.recipeDetailForm.get("name")
+  }
+  get servings() {
+    return this.recipeDetailForm.get("servings")
+  }
+  get description() {
+    return this.recipeDetailForm.get("description")
+  }
+  get instructions() {
+    return this.recipeDetailForm.get("instructions")
+  }
+  get notes() {
+    return this.recipeDetailForm.get("notes")
+  }
 }
