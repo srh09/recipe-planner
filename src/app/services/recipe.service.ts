6006cf28a99632cd1f7f8a7569a34ecb3f5af372
SRH <srholand@gmail.com>
2020-06-23 21:35:49 -0500

    added demo data

--- src/app/services/recipe.service.ts
+++ src/app/services/recipe.service.ts
@@ -1,21 +1,28 @@
 import { Injectable } from "@angular/core"
 import { Recipe } from "../models/recipe.model"
 import { Subject } from "rxjs"
+import { Ingredient } from "../models/ingredient.model"
 
 @Injectable({
-  providedIn: "root",
+  providedIn: "root"
 })
 export class RecipeService {
   recipesChanged = new Subject<Recipe[]>()
 
+  private ingredients: Ingredient[] = [
+    new Ingredient("Tomato", 3),
+    new Ingredient("Cheese", 4),
+    new Ingredient("Flour", 2)
+  ]
   private recipes: Recipe[] = [
     new Recipe(
       "Pizza",
       3,
       "This is a description of Pizza, text will go here...",
       "recipe pizza notes will go on this line",
-      ["step one text", "step two text", "step three instructions"]
-    ),
+      ["step one text", "step two text", "step three instructions"],
+      [this.ingredients[0], this.ingredients[1], this.ingredients[2]]
+    )
   ]
 
   constructor() {}
